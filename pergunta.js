a5a1ed26cf999ecfa21d92168fec50a660287c1b
anaju-guligurski <140830132+anaju-guligurski@users.noreply.github.com>
2024-10-29 08:48:25 -0300

    .

---
+++ pergunta.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Matemática'
+    'O que é um Triângulo Equilátero?'
+    'É quando ele possui a medida dos três lados congruentes'
+)
+criaCartao(
+    'Biotecnologia'
+    'Qual a cor da biotecnologia que fabrica medicamentos?'
+    'A biotecnologia vermelha'
+)
+criaCartao(
+    'Bioloia'
+    'Qual a função da mitocôndria?'
+    'A função da mitocôndria de gerar energia '
+)
+criaCartao(
+    'Química'
+    'Do que as cadeias carbônicas são contituídas?'
+    'As cadeias carbônicas são constituídas pelas ligações entre os átomos de carbono e hidrogênio'
+)
+criaCartao(
+    'História'
+    'Qual foi o principal motivo do começo da Segunda Guerrra Mundial?'
+    'A invasão nazista da Polônia'
+)
+criaCartao(
+    'Geografia'
+    'O que é um continente?'
+    'Os continentes são grandes extensões de terra emersas, limitadas pelas águas de mares e oceanos'
+)
+criaCartao(
+    'Português'
+    'O que é um conto?'
+    'O conto é um gênero literário marcado pela concisão. Tais narrativas têm, em geral, poucos personagens, espaço e tempo restritos e um conflito único
+)
+criaCartao(
+    'Sociologia'
+    'O que a Sociologia estuda?'
+    'A Sociologia estuda a sociedade, ou seja, estuda o comportamento humano'
+)
+criaCartao(
+    'Inglês'
+    'Qual a tradução de "the books on the table?"'
+    'O livro está sobre a mesa'
+)
+criaCartao(
+    'Educação Financeira'
+    'O que são açoẽs?'
+    'Ações representam frações de companhias de capital aberto, ou seja, aquelas empresas que são negociadas na Bolsa de Valores'
+)
+criaCartao(
+    'Projeto de vida'
+    'O que é o Projeto de Vida?'
+    'O Projeto de Vida é uma iniciativa cujo objetivo é criar uma ponte entre quem o aluno é e quem ele quer ser'
+)
+criaCartao(
+    'Programação'
+    'O que é HTML?'
+    ' é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web'
+
+criaCartao(
+    'Robótica'
+    'O que é um potenciômetro?'
+    'O potenciômetro é um tipo de resistor ajustável'
+)
+criaCartao(
+    'Empreendedorismo'
+    'O que é a CLT?'
+    'Consolidação das Leis do Trabalho'
+)
