8c7e144c5b08c3d3fd7e72f0fb15185ff96273ea
anaju-guligurski <140830132+anaju-guligurski@users.noreply.github.com>
2024-11-05 08:16:41 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+     <div class="cartao_conteudo">
+                    <h3>${categotia}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${resposta}</p>
+                    </div>
+
+                </div>
+    `
+    let container = document. getElementById('container')
+    container.appendChild(cartao)
 }
